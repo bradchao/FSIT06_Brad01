7367826b1c3bc7b84b90019f5abd300266d80b56
Brad <brad@brad.tw>
2019-07-21 11:13:36 +0800

    201907211112

---
+++ app/src/main/java/tw/org/iii/appps/brad3/MainActivity.java
@@ -0,0 +1,14 @@
+package tw.org.iii.appps.brad3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
